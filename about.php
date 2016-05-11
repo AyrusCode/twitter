d7698ea622d91d9086800ae6d73622acc7e0deec
Denis Khavin <denis.khavin@ucc.on.ca>
2016-05-11 10:29:24 -0400

    Created a div class for each new paragraph

--- about.php
+++ about.php
@@ -12,12 +12,33 @@
 
 <meta name="author" content="Denis Khavin">
 </head>
+<style>
+div.headers {
+    background-color:black;
+    color:white;
+    margin:20px;
+    padding:20px;
+}
+</style>
 <body>
    <h1> <b> <center> About <i>Relay</i> </center> </b> </h1>
+   <div class="headers">
    <h2> What is this cheap Twitter knockoff? </h2>
-   <p> <i>Relay</i> is a project we have been working on in our Grade 10 computer science class.
+   <p1> <i>Relay</i> is a project we have been working on in our Grade 10 computer science class.
+        We started off with a table and a database and edit the sample code until we could login,
+        add tweets, add hashtags, search tweets, etc.
+   </p1>
+  </div>
+  <div class="headers">
+  <h3> Who are the creators of <i>Relay</i>? </h3>
+  <p2> <i>Relay</i> was created by Zack Nathan, Surya Pandiaraju, Michael McGovern, and Denis Khavin. </p2>
+  </div>
+  <br>
+  <h3> About Zach Nathan </h3>
 
-   </p>
+
+
+
 </body>
 </html>
 
