fc6b4b3fbb3a96794ca82cbec8067bf7d2fe190b
Denis Khavin <denis.khavin@ucc.on.ca>
2016-05-11 10:01:02 -0400

    Added very basic page with a header and text

--- about.php
+++ about.php
@@ -1,1 +1,23 @@
+<!doctype html>
+<head>
+<meta charset="utf-8">
+
+<!-- the title tag should be 60 characters or less -->
+<title>About <i>Relay</i> </title>
+
+
+<!-- meta description must be limited to 160 characters or less -->
+<meta name="<i>Relay</i> description" content="About our Twitter project.">
+
+
+<meta name="author" content="Denis Khavin">
+</head>
+<body>
+   <h1> <b> <center> About <i>Relay</i> </center> </b> </h1>
+   <h2> What is this cheap Twitter knockoff? </h2>
+   <p> <i>Relay</i> is a project we have been working on in our Grade 10 computer science class.
+
+   </p>
+</body>
+</html>
 
