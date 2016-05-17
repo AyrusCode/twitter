8db42632871edf73bb592b1186d39d168cf4c920
ZackNathan <zack.nathan@ucc.on.ca>
2016-05-17 00:23:00 -0400

    Added comments and a nicer error message

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<!--
+    Enlighten: login.php
+    Use this page to login to Enlighten to view the feed
+    Created by Zack Nathan, Denis Khavin, Surya Pandiaraju, Michael McGovern, and Mark Hoel
+    Created and last edited in May 2016
+-->
+
 <?php
 
     // First we execute our common code to connection to the database and start the session
@@ -31,14 +38,11 @@
             ':username' => $_POST['username']
         );
 
-        try
-        {
+        try {
             // Execute the query against the database
             $stmt = $db->prepare($query);
             $result = $stmt->execute($query_params);
-        }
-        catch(PDOException $ex)
-        {
+        } catch(PDOException $ex) {
             // Note: On a production website, you should not output $ex->getMessage().
             // It may provide an attacker with helpful information about your code.
             die("Failed to run query: " . $ex->getMessage());
@@ -52,19 +56,16 @@
         // Retrieve the user data from the database.  If $row is false, then the username
         // they entered is not registered.
         $row = $stmt->fetch();
-        if($row)
-        {
+        if ($row) {
             // Using the password submitted by the user and the salt stored in the database,
             // we now check to see whether the passwords match by hashing the submitted password
             // and comparing it to the hashed version already stored in the database.
             $check_password = hash('sha256', $_POST['password'] . $row['salt']);
-            for($round = 0; $round < 65536; $round++)
-            {
+            for ($round = 0; $round < 65536; $round++) {
                 $check_password = hash('sha256', $check_password . $row['salt']);
             }
 
-            if($check_password === $row['password'])
-            {
+            if($check_password === $row['password']) {
                 // If they do, then we flip this to true
                 $login_ok = true;
             }
@@ -72,8 +73,8 @@
 
         // If the user logged in successfully, then we send them to the private members-only page
         // Otherwise, we display a login failed message and show the login form again
-        if($login_ok)
-        {
+        if($login_ok) {
+
             // Here I am preparing to store the $row array into the $_SESSION by
             // removing the salt and password values from it.  Although $_SESSION is
             // stored on the server-side, there is no reason to store sensitive values
@@ -89,12 +90,15 @@
             $_SESSION['user'] = $row;
 
             // Redirect the user to the private members-only page.
-            header("Location: feed.php");
-            die("Redirecting to: feed.php");
+            $location = "http://".$_SERVER['HTTP_HOST']."/twitter/feed.php";
+            echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
         }
-        else
-        {
+
+        else {
             // Tell the user they failed
+            // This is done by passing a url parameter
+            $location = "http://".$_SERVER['HTTP_HOST']."/twitter/login.php?errors=loginfailed";
+            echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
         }
     }
 
@@ -106,6 +110,10 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link href="dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="enlighten.css">
+
+        <meta charset="UTF-8">
+        <meta name="description" content="Login to Enlighten">
+        <meta name="author" content="Zack, Denis, Surya, and Michael">
     </head>
 
     <body background="resources/tower.jpg">
@@ -117,15 +125,24 @@
             </div>
 
             <br>
-            <div class="container2">
+            <div class="redbox">
 
                 <font color ="white"><h2>Login</h2></font>
-                <form action="login.php" method="post">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
                     <br />
-                    <input type="text" name="username" value="<?php echo $submitted_username; ?>" class="form-control" placeholder="Username" aria-describedby="sizing-addon1"/>
+                    <input type="text" name="username" value="<?php echo $submitted_username; ?>" class="form-control" placeholder="Username" aria-describedby="sizing-addon1" required/>
                     <br />
-                    <input type="password" name="password" value="" class="form-control" placeholder="Password" aria-describedby="sizing-addon1"/>
+                    <input type="password" name="password" value="" class="form-control" placeholder="Password" aria-describedby="sizing-addon1" required/>
+                    <br />
+                    <?php
+                    // Display the message if the url contains the loginfailed error
+                    // This means the user entered incorrect login credentials
+                    if (strpos($_GET["errors"], "loginfailed") !== false) {
+                        // convenient link to the register page
+                        echo "<span style='color: white;'>Invalid username/password combination, try again or <a href='register.php'><strong><span style='color:white'>register</span></a></span><br>";
+                    }
+                    ?>
                     <br />
 
                    <center> <button class="button" style="vertical-align:middle"><span>Enter the Firepit</span> </button></center>
@@ -133,7 +150,7 @@
                 </form>
 
                 <a href="register.php"><strong><span style="color:white">Don't have an account? Register here!</span><br /></a>
-                <a href="about.php"><strong><span style="color:white">About Enlighten</span></a>
+                <a href="about.php"><strong><span style="color:white">About Enlighten and FAQ</span></a>
             </div>
         </div>
     </body>
