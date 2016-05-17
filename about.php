f329b0dcde7f7c520dfd5986111766c763f7bf12
ZackNathan <zack.nathan@ucc.on.ca>
2016-05-16 23:00:39 -0400

    Is now an about page and a help page. Reformatted also

--- about.php
+++ about.php
@@ -23,36 +23,91 @@
 
                     <p style="text-align: left;">
                         Enlighten is a project we have been working on in our Grade 10 computer science class.
-                        We started off with a table and a database and edit the sample code until we could
-                        add "tweets", add/search hashtags, and search users. etc. Tools used were IDEs such as Atom and Adobe
-                        Dreamweaver CS6. In terms of programming languages used, we used CSS, HTML, and PHP to do all of our
-                        editing/programming.
+                        We started off with a sample project where we could add and remove rows from an SQL database.
+                        We edited the sample code until we could add "tweets" and search for hashtags and users.
+                        We also created a beautiful website to go with it.
+                        <br><br>
+                        The website uses HTML5 and CSS for the frontend, and PHP and SQL for the backend.
+                        Bootstrap, StackOverFlow, W3Schools, Github, and Google were all extremely helpful resources and we
+                        could not have made this website without them.
+                        <br><br>
+                        The programming was all done in two IDEs, Atom and Adobe Dreamweaver CS6. Each developer used
+                        the IDE of their choice. MAMP was used to manage the website and run the MySQL and Apache servers.
+                        Enlighten was made with and optimized for Google Chrome.
+                        <br><br>
+                        Creating this website was an excellent and enjoyable learning experience for all of the developers.
+                        We started with little to no knowledge of any of the tools or languages involved.
+                        Now all of us have the necessary skills to create and manage our own websites.
                     </p>
 
-                    <h2>Meet the Team!</h2>
+                    <br>
+                    <h2>FAQ and Help</h2>
 
                     <p style="text-align: left;">
-                        <center><img src="https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12400968_928800447186729_4816870285512992609_n.jpg?oh=3c24135ad59d0e8a79ad8c349900a656&oe=57D941AA"  style="width:304px;height:304px;"></center><br>
-                       <h3> Zack Nathan:<br> zack.nathan@ucc.on.ca<br></h3>
+                        <strong>Why are there no tweets?</strong>
+                        <br>
+                        Because you just downloaded an empty version of the website! With the release, we provided
+                        an SQL database import script with a sample database of tweets and users for you to use.
+                        <br><br>
+                        <strong>Can I search for a user and a hashtag at the same time?</strong>
+                        <br>
+                        Yes, you absolutely can! Just type each search term into the correct box and click the GO!
+                        button on either one of them. Any new search will automatically clear the old search.
+                        <br><br>
+                        <strong>Where did you find these beautiful backgrounds?</strong>
+                        <br>
+                        These backgrounds are all screenshots from a game called Firewatch. If you are interested,
+                        The game is available on steam, and you can visit their website at www.firewatchgame.com
+                        <br><br>
+                        <strong>I love the website so much! Can I hire you guys?</strong>
+                        <br>
+                        Of course you can! Contact us using the emails provided below.
+                        <br><br>
+                        <strong>Question not found? Email a developer!</strong>
+                    </p>
+
+                    <br>
+                    <h2>Meet the Team</h2>
+
+                    <p>
+                    <h4>Zack Nathan</h4>
+                    <h5>zack.nathan@ucc.on.ca</h5><br>
+                    <img width=200px src="resources/zack.jpg"><br><br>
+                    <p style="text-align: left;">
                         Our head programmer, Zack really brought the team together by using his
-                        ingenious Computer Science skills to program the website. He is deserving
-                        of the title "Genius". <br><br>
-                        <center> <img src="https://doc-08-bc-docs.googleusercontent.com/docs/securesc/m58bm8tfao09shj0st5rip2h45j8sq7q/2f5cm2dig4fuf1pfc9114q0ccnlqqtr9/1463443200000/08632977721032377366/03782313742957264096/0B-67aGL87ij3R0RGT1dra0JWTXc?e=view"  style="width:304px;height:458px;"></center><br>
-                        <h3>Surya Pandiaraju: surya.pandiaraju@ucc.on.ca<br></h3>
+                        Computer Science skills to program the back end website. He wrote most of
+                        the PHP and created and managed the SQL database. He was still able to make
+                        great contributions to the website despite missing classes and limiting
+                        his computer time due to a concussion.<br><br>
+                    </p>
+
+                    <h4>Denis Khavin</h4>
+                    <h5>denis.khavin@ucc.on.ca</h5><br>
+                    <img width=200px src="resources/denis.jpg"><br><br>
+                    <p style="text-align: left;">
+                        Another member of the team, Denis dealt with project management more than anything
+                        else, making sure everyone was on task. He held the scrum meeting and was key in
+                        deciding the agenda for the day and the tasks everyone was to complete. He is
+                        also the one presenting to you right now!<br><br>
+                    </p>
+
+                    <h4>Surya Pandiaraju</h4>
+                    <h5>surya.pandiaraju@ucc.on.ca</h5>
+                    No image provided<br>
+                    <img width=200px src="resources/surya.png"><br><br>
+                    <p style="text-align: left;">
                         The web designer, with CSS and doing a bit of brain-storming and programming himself,
                         Surya had great contributions to the group. He was able to keep everything organized,
-                        especially when it was needed.<br><br>
-                         <center><img src="https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/1505495_1608530606096764_1662616365825140472_n.jpg?oh=d57f932e7e85214b823a6325ac1bef75&oe=57D55C40"  style="width:304px;height:304px;"></p></center><br>
-                        <h3>Michael McGovern: michael.mcGovern@ucc.on.ca<br></h3>
-                        Another web designer, Michael was a great member of the team. His great sense of 
-                        humour, and his eye for design with previous projects made him an essential member 
-                        of the Enlighten! Team.<br><br>
-                         <center><img src="https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12729279_1232556456772273_6925991321622019273_n.jpg?oh=3df3f666b5157d0817321d5b849e7b7b&oe=57A1E75B"  style="width:304px;height:508px;"></center><br>
-                        <h3>Denis Khavin:<br> denis.khavin@ucc.on.ca<br></h3>
-                        Another member of the team, Denis dealt with project management more than anything 
-                        else, making sure everyone was on task. He held the scrum meeting and was key in 
-                        deciding the agenda for the day and the tasks everyone was to complete. He is
-                        also the one presenting and preparing the presentation today!<br><br>
+                        especially when it was needed. Surya also wrote these developer descriptions!<br><br>
+                    </p>
+
+                    <h4>Michael McGovern</h4>
+                    <h5>michael.mcgovern@ucc.on.ca</h5><br>
+                    <img width=200px src="resources/michael.jpg"><br><br>
+                    <p style="text-align: left;">
+                        Another web designer, Michael was a great member of the team. His great sense of
+                        humour, and his eye for design with previous projects made him an essential member
+                        of the Enlighten Team.<br><br>
                     </p>
 
                 </font>
