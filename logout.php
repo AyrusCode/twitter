42def6b68bf988b8810f424bb5c445fdb4cbae6f
ZackNathan <zack.nathan@ucc.on.ca>
2016-05-16 23:30:12 -0400

    It didn't work so I fixed it

--- logout.php
+++ logout.php
@@ -1,13 +1,20 @@
-<?php 
+<!--
+    Enlighten: logout.php
+    Script to log the user out of Enlighten
+    Created by Zack Nathan, Denis Khavin, Surya Pandiaraju, Michael McGovern, and Mark Hoel
+    Created and last edited in May 2016
+-->
 
-    // First we execute our common code to connection to the database and start the session 
-    require("common.php"); 
-     
-    // We remove the user's data from the session 
-    unset($_SESSION['user']); 
-     
-    // We redirect them to the login page 
-    header("Location: login.php"); 
-    die("Redirecting to: login.php");
-	
+<?php
+
+    // First we execute our common code to connection to the database and start the session
+    require("common.php");
+
+    // We remove the user's data from the session
+    unset($_SESSION['user']);
+
+    // We redirect them to the login page
+    $location = "http://".$_SERVER['HTTP_HOST']."/twitter/login.php";
+    echo '<META HTTP-EQUIV="refresh" CONTENT="0;URL='.$location.'">';
+
 ?>
